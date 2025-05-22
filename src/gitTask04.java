502471c3f95cf19dc84cf86958b7d5db4244e9ac
SametEmsen <sametemsen@gmail.com>
2025-05-22 22:27:07 +0300

    gitTask04 eklendi

---
+++ src/gitTask04.java
@@ -0,0 +1,7 @@
+public class gitTask04 {
+    public static void main(String[] args) {
+        System.out.println("Api testleri önemlidir");
+        System.out.println("SQL bilmek gerekir");
+        System.out.println("CI/CD süreçleri artık işimizi çok kolaylaştırdı");
+    }
+}
