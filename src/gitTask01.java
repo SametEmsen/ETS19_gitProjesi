e6e5bb3aa5d87f0a353a75e920734aec9a00d57a
SametEmsen <sametemsen@gmail.com>
2025-05-22 21:43:58 +0300

    proje güncellendi

--- src/gitTask01.java
+++ src/gitTask01.java
@@ -1,5 +1,6 @@
 public class gitTask01 {
     public static void main(String[] args) {
         System.out.println("Java candır");
+        System.out.println("gerisi yalandır");
     }
 }
--- src/gitTask02.java
+++
@@ -1,5 +0,0 @@
-public class gitTask02 {
-    public static void main(String[] args) {
-        System.out.println( "gerisi yalandır");
-    }
-}
