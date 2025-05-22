439850ddb3db531fd4a75221c6034ca5bda7d8ba
SametEmsen <146114142+SametEmsen@users.noreply.github.com>
2025-05-22 22:09:35 +0300

    Update gitTask01.java

--- src/gitTask01.java
+++ src/gitTask01.java
@@ -2,5 +2,6 @@ public class gitTask01 {
     public static void main(String[] args) {
         System.out.println("Java candır");
         System.out.println("gerisi yalandır");
+        System.out.println("fani dünya");
     }
 }
